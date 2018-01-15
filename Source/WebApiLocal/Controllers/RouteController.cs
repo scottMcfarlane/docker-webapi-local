76c87c0bc3f6c354b9bb0bbeda23f9555d2b63db
Scott Mcfarlane <scottm@pageuppeople.com>
2018-01-15 16:32:13 +1100

    Adds initial web api project

---
+++ Source/WebApiLocal/Controllers/RouteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiLocal.Controllers
+{
+    [Route("/")]
+    public class RouteController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get(string route)
+        {
+            return Ok(route);
+        }
+
+        [HttpPost]
+        public IActionResult Post(string route)
+        {
+            return Ok(route);
+        }
+
+        [HttpPut]
+        public IActionResult Put(string route)
+        {
+            return Ok(route);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string route)
+        {
+            return Ok(route);
+        }
+    }
+}
---
+++ Source/WebApiLocal/SampleActionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using WebApiLocal.Controllers;
+
+namespace WebApiLocal
+{
+    public class SampleActionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            System.Console.WriteLine("Oh Hello There");
+            var myController = (RouteController)context.Controller;
+            //context.Result = myController.RedirectToAction(context.HttpContext.Request.Method, myController);
+            context.Result = myController.Get(myController.RouteData.ToString());
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //Do nothing
+        }
+    }
+}
